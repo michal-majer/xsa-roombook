e47e2026577cca55f2ccf974a282d7b7f8106ade
michal.majer <michal.majer@kantwert.de>
2020-02-05 23:13:39 +0000

    changes

--- ui5/resources/webapp/controller/Index.controller.js
+++ ui5/resources/webapp/controller/Index.controller.js
@@ -1,17 +1,65 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/unified/library"
-], function (Controller, JSONModel, unifiedLibrary) {
+	"sap/ui/unified/library",
+	"../model/formatter"
+], function (Controller, JSONModel, unifiedLibrary, formatter) {
 	"use strict";
 
 	var CalendarDayType = unifiedLibrary.CalendarDayType;
 
 	return Controller.extend("roombook.ui5.controller.Index", {
+		formatter: formatter,
 		onInit: function () {
-			var oModel = new JSONModel();
-			oModel.setData({
-				startDate: new Date("2018", "6", "9"),
+			this._setApiOdata();
+			this._setTimeModel();
+			this._setFilterOnTodayMettingTiles();
+			this._checkForUpdates();
+		},
+
+		_setApiOdata: function () {
+			const oApi = new sap.ui.model.odata.v4.ODataModel({
+				groupId: "$direct",
+				synchronizationMode: "None",
+				operationMode: "Server",
+				serviceUrl: "/api/",
+			});
+
+			this.getView().setModel(oApi);
+		},
+
+		_setFilterOnTodayMettingTiles: function () {
+			let startOfDay = new Date();
+			let endOfDay = new Date();
+
+			startOfDay.setHours(0, 0, 0);
+			endOfDay.setHours(23, 59, 59);
+
+			const dateTimeOffsetFormater = new sap.ui.model.odata.type.DateTimeOffset({}, {
+				V4: true
+			});
+			const oDataStartOfDay = dateTimeOffsetFormater.parseValue(startOfDay, "object");
+			const oDataEndOfDay = dateTimeOffsetFormater.parseValue(endOfDay, "object");
+
+			const oRoomFilter = new sap.ui.model.Filter("room_ID", "EQ", "f5f96661-bd88-4a8a-866d-750aa865ebd0");
+
+			const oTodayFilter = new sap.ui.model.Filter({
+				path: 'startAt',
+				operator: sap.ui.model.FilterOperator.BT,
+				value1: oDataStartOfDay,
+				value2: oDataEndOfDay
+			});
+
+			const oFlexBox = this.getView().byId("todayMettingTiles");
+			oFlexBox.getBinding("items").filter([oRoomFilter, oTodayFilter]);
+		},
+
+		_setTimeModel: function () {
+			const oTimeModel = new JSONModel();
+
+			oTimeModel.setData({
+				startDate: new Date(),
+				currentTime: this._getCurrentTime(),
 				types: (function () {
 					var aTypes = [];
 					for (var key in CalendarDayType) {
@@ -20,40 +68,32 @@ sap.ui.define([
 						});
 					}
 					return aTypes;
-				})(),
-				appointments: [{
-					title: "Meet John Miller",
-					type: CalendarDayType.Type05,
-					startDate: new Date("2018", "6", "8", "5", "0"),
-					endDate: new Date("2018", "6", "8", "6", "0")
-				}]
+				})()
 			});
-			this.getView().setModel(oModel);
-			
-			const oTimeModel = new JSONModel();
-			
-			oTimeModel.setData({
-				currentTime: this._getCurrentTime()
-			});
+
 			this.getView().setModel(oTimeModel, "timeModel");
-			
 			this._changeCurrentTime();
-			console.log(this.getView().getModel("api"));
 		},
-		
-		_getCurrentTime: function() {
+
+		_getCurrentTime: function () {
 			const now = new Date();
 			return `${now.getHours()}:${('0' +now.getMinutes()).slice(-2)}`;
 		},
-		
-		_changeCurrentTime: function() {
+
+		_changeCurrentTime: function () {
 			setTimeout(() => {
 				const oTimeModel = this.getView().getModel("timeModel");
 				oTimeModel.setProperty("/currentTime", this._getCurrentTime());
 				setTimeout(this._changeCurrentTime(), 1000);
 			}, 1000);
+		},
+
+		_checkForUpdates: function () {
+			setTimeout(() => {
+				this.getView().getModel().refresh();
+				setTimeout(this._checkForUpdates(), 60000);
+			}, 60000);
 		}
-		
-		
+
 	});
 });
---
+++ ui5/resources/webapp/model/formatter.js
@@ -0,0 +1,9 @@
+sap.ui.define([], function () {
+	"use strict";
+	return {
+		tileState: function (startAt, endAt) {
+			console.log(startAt, endAt);
+			return 'Disabled';
+		}
+	};
+});
