66f5857343d999a18d836d68bc82716d4b0958b9
Michał Majer <michal.majer@hotmail.com>
2020-02-12 22:07:42 +0100

    formatter - disable generic tile, when meeting is over

--- ui5/resources/webapp/model/formatter.js
+++ ui5/resources/webapp/model/formatter.js
@@ -2,8 +2,22 @@ sap.ui.define([], function () {
 	"use strict";
 	return {
 		tileState: function (startAt, endAt) {
-			console.log(startAt, endAt);
-			return 'Disabled';
+			const now = new Date();
+
+			const startAtDate = new Date();
+			const endAtDate = new Date();
+			
+			let [hours, minutes, seconds] = startAt.split(':');
+			startAtDate.setHours(+hours);
+			startAtDate.setMinutes(minutes);
+			startAtDate.setSeconds(seconds);
+
+			[hours, minutes, seconds] = endAt.split(':');
+			endAtDate.setHours(+hours);
+			endAtDate.setMinutes(minutes);
+			endAtDate.setSeconds(seconds);		
+			
+			return startAtDate >= now && now <= endAtDate ? 'Loaded' : 'Disabled';
 		}
 	};
 });
